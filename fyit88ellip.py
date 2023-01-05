18d8ea26404103b95a3284f5eec8142aa42a6c0d
swayam-gupta31 <121996258+swayam-gupta31@users.noreply.github.com>
2023-01-05 09:46:38 +0530

    Add files via upload

---
+++ fyit88ellip.py
@@ -0,0 +1,15 @@
+import math
+class ellipse:
+    a=float(input("enter axix1"))
+    b=float(input("enter axix2"))
+    def area(self):
+        print("area of ellipse is",3.14*self.a*self.b)
+        def perimeter(self):
+            print("perimeter of ellipse is=",2*3.14*math.sqrt((self.a*self.b*self.b)/(2)))
+
+
+
+c=ellipse()
+x=c.area()
+y=c.perimeter
+                                            
